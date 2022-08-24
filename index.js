6cf7977a8fde7b9fd1d1e2471b883343fd384b49
LeeYitian <108401455+LeeYitian@users.noreply.github.com>
2022-08-24 18:14:04 +0800

    完成RWD，考慮.intro位置

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 //loader
-// (function (){
-//     const fragment = document.createDocumentFragment();
-//     const loader = document.createElement("div");
-//     loader.className = "loader";
-//     const welcome = document.createElement("h3");
-//     welcome.textContent = "WELCOME";
-//     loader.appendChild(welcome);
-//     for(let i = 1; i<6;i++){
-//         const dot = document.createElement("div");
-//         dot.className = "dot";
-//         loader.appendChild(dot);
-//     }
-//     fragment.appendChild(loader);
-//     const wrap = document.querySelector(".wrap");
-//     document.querySelector("body").insertBefore(fragment,wrap);
-//     setTimeout(()=>{
-//         loader.style.opacity = 0;
-//         loader.style.display = "none";
-//         wrap.style.display = "block";
-//         setTimeout(()=>wrap.style.opacity = 1,50)
-//     },3000)
-// })();
+(function (){
+    const fragment = document.createDocumentFragment();
+    const loader = document.createElement("div");
+    loader.className = "loader";
+    const welcome = document.createElement("h3");
+    welcome.textContent = "WELCOME";
+    loader.appendChild(welcome);
+    for(let i = 1; i<6;i++){
+        const dot = document.createElement("div");
+        dot.className = "dot";
+        loader.appendChild(dot);
+    }
+    fragment.appendChild(loader);
+    const wrap = document.querySelector(".wrap");
+    document.querySelector("body").insertBefore(fragment,wrap);
+    setTimeout(()=>{
+        document.querySelector("body").removeChild(loader);
+        createBackground();
+        wrap.style.display = "block";
+        setTimeout(()=>{wrap.style.opacity = 1;},800)
+    },2000)
+})();
 
 //background
 function createBackground(){
     const fragment = document.createDocumentFragment();
     const background = document.createElement("div");
     background.className = "background";
-    for(let i = 0; i<6;i++){
+    for(let i = 0; i<8;i++){
         const bg_row = document.createElement("div");
         bg_row.className = "bg_row";
         for (let r = 0; r<21; r++){
@@ -40,6 +40,16 @@ function createBackground(){
     fragment.appendChild(background);
     document.querySelector("body").appendChild(fragment);
 }
-createBackground();
 
+
+//menu
+const menu = document.querySelector("nav");
+const body = document.querySelector("body");
+body.addEventListener("click",()=>menu.classList.remove("show"));
+menu.addEventListener("click",showMenu);
+function showMenu(e){
+    if(e.target.closest("ul"))return;
+    this.classList.toggle("show");
+    e.stopPropagation();
+}
 
